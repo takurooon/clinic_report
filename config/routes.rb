da0288b3e4c227e0f360ec9c7afce39e34c419d3
takurooon <takurokodama0726@gmail.com>
2019-10-21 15:32:21 +0900

    generate devise:users controller, add my_page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
 
   root 'reports#index'
   
-  devise_for :users
+  devise_for :users, controllers: {
+   registrations: 'users/registrations',
+   sessions: 'users/sessions',
+   passwords: 'users/passwords',
+   confirmations: 'users/confirmations'
+  }
+
+  devise_scope :user do
+    get 'my_page' => 'users/registrations#my_page'
+  end
 
   resources :users, shallow: true do
     resources :reports
