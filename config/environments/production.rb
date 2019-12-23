028e0ccf9a3bd9ba71a7bf863e90485242cc0e01
takurooon <takurokodama0726@gmail.com>
2019-12-23 15:41:43 +0900

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,17 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # 追加
+  config.action_mailer.default_url_options = { host: 'https://clinic-report.herokuapp.com/reports' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: "smtp.SendGrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
