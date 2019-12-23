2e07c434a4b82698f04097cf23a47f38e14e5ed4
takurooon <takurokodama0726@gmail.com>
2019-12-23 15:55:15 +0900

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'my_page/index'
   root 'reports#home'
+  get 'my_page/index'
   
   devise_for :users, controllers: {
     registrations: 'users/registrations',
