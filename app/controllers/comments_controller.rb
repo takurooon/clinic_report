bc94a0f0a1007332242864be15c7abc42fa48cd2
takurooon <takurokodama0726@gmail.com>
2020-01-18 22:34:07 +0900

    add snsbar

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,10 @@ class CommentsController < ApplicationController
 
   def destroy
     comment = Comment.find(params[:id])
+    if comment.user != current_user
+      redirect_to report_path, alert: '削除権限がありません' 
+      return
+    end
     comment.delete
     redirect_to comment.report, flash: { notice: "コメントが削除されました" }
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,5 +51,4 @@ module ApplicationHelper
       }
     }
   end
-
 end
