349cef62bd2894beba45734a5e965d3049ab2f17
takurooon <takurokodama0726@gmail.com>
2020-10-20 20:37:18 +0900

    showのコメント欄へのjsによるページ内リンク

--- app/assets/javascript/report_form.js
+++ app/assets/javascript/report_form.js
@@ -602,3 +602,16 @@ $(function (){
 //     updateTextView($(this));
 //   });
 // });
+
+// コメント欄へのページ内リンク
+$(function(){
+  $('a[href^="#comments_area"]').click(function(){
+    var adjust = 50;
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - adjust;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
