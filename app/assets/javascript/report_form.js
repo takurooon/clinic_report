9edd5031e791f53ada67d64c7573701172b374e3
takurooon <takurokodama0726@gmail.com>
2020-10-22 09:51:23 +0900

    title入力欄下に文字数カウント機能実装

--- app/assets/javascript/report_form.js
+++ app/assets/javascript/report_form.js
@@ -550,7 +550,7 @@ function radioDeselection(already, numeric) {
   }
 }
 
-// 文字数カウント
+// コメント文字数カウント
 $(function (){
   var count = $(".js-text").text().replace(/\n/g, "改行").length;
   var now_count = 500 - count;
@@ -573,6 +573,32 @@ $(function (){
       var status = "残り" + now_count + "文字"
     }
     $(".js-text-count").text(status);
+  });
+});
+
+// title文字数カウント
+$(function (){
+  var count = $(".title-js-text").text().replace(/\n/g, "改行").length;
+  var now_count = 64 - count;
+  if (count > 64) {
+    $(".title-js-text-count").css("color","red");
+  } else {
+    $(".title-js-text-count").css("color","#808080");
+  }
+  $(".title-js-text-count").text( "残り" + now_count + "文字");
+
+  $(".title-js-text").on("keyup", function() {
+    var count = $(this).val().replace(/\n/g, "改行").length;
+    var now_count = 64 - count;
+
+    if (count > 64) {
+      $(".title-js-text-count").css("color","red");
+      var status = (now_count * -1) + "文字オーバーしています！"
+    } else {
+      $(".title-js-text-count").css("color","#808080");
+      var status = "残り" + now_count + "文字"
+    }
+    $(".title-js-text-count").text(status);
   });
 });
 
