deeeb1c5ebd65e5f94bc508c91b800dbc15b1b68
takurooon <takurokodama0726@gmail.com>
2020-11-05 14:53:16 +0900

    クリニックページのスコア桁数変更

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -34,12 +34,12 @@ class ClinicsController < ApplicationController
     @clinic_reports = clinic_reports.limit(@get_limit).order(created_at: :desc)
     @clinic_reports_count = Report.where(clinic_id: @clinic, status: 0).count
 
-    @doctor_quality = clinic_reports.average(:doctor_quality).to_f
-    @staff_quality = clinic_reports.average(:staff_quality).to_f
-    @impression_of_technology = clinic_reports.average(:impression_of_technology).to_f
-    @impression_of_price = clinic_reports.average(:impression_of_price).to_f
-    @average_waiting_time2 = clinic_reports.average(:average_waiting_time2).to_f
-    @comfort_of_space = clinic_reports.average(:comfort_of_space).to_f
+    @doctor_quality = clinic_reports.average(:doctor_quality).round(1)
+    @staff_quality = clinic_reports.average(:staff_quality).round(1)
+    @impression_of_technology = clinic_reports.average(:impression_of_technology).round(1)
+    @impression_of_price = clinic_reports.average(:impression_of_price).round(1)
+    @average_waiting_time2 = clinic_reports.average(:average_waiting_time2).round(1)
+    @comfort_of_space = clinic_reports.average(:comfort_of_space).round(1)
     @average_score = ((@doctor_quality + @staff_quality + @impression_of_technology + @impression_of_price + @average_waiting_time2 + @comfort_of_space)/6.to_f).round(1)
     gon.clinic_name = @clinic.name
     gon.clinic_evaluation = []
