115ad7feb3962e8e6f1cfeb5a831b23bcc213525
takurooon <takurokodama0726@gmail.com>
2020-11-22 15:40:29 +0900

    admin/reports/show作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < Admin::ApplicationController
   def show
     @user = User.find(params[:id])
+    @reports = @user.reports.order("created_at DESC").page(params[:page]).per(10)
   end
 end
