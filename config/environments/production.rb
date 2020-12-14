610fd3b6120be74ea8c97dea6525e59b96f07233
takurooon <takurokodama0726@gmail.com>
2020-12-15 08:47:27 +0900

    config/environments/production.rbを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'clinic-report.herokuapp.com'
+  host = 'repoco.net'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
