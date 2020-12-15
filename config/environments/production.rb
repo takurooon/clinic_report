c98e29f582ff8e0b05af7ca801dc8fb865882d44
takurooon <takurokodama0726@gmail.com>
2020-12-15 10:14:20 +0900

    config/enviroments/production.rbを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,15 +112,13 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # 追加
-  config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   host = 'clinic-report.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "repoco.net",
+    domain: "heroku.com",
     address: "smtp.SendGrid.net",
     port: 587,
     authentication: :plain,
