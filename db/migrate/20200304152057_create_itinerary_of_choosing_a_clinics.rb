f28e944edf6f04d24c08655e7dc0e2a8fce7c302
takurooon <takurokodama0726@gmail.com>
2021-01-28 16:43:10 +0900

    転院歴モデル(マイグレーションファイル)からnull制約を削除

--- db/migrate/20200304152057_create_itinerary_of_choosing_a_clinics.rb
+++ db/migrate/20200304152057_create_itinerary_of_choosing_a_clinics.rb
@@ -2,7 +2,7 @@ class CreateItineraryOfChoosingAClinics < ActiveRecord::Migration[6.0]
   def change
     create_table :itinerary_of_choosing_a_clinics do |t|
       t.references :report, null: false, foreign_key: true
-      t.references :clinic, null: false, foreign_key: true
+      t.references :clinic, foreign_key: true
       t.integer :order_of_transfer
 
       t.timestamps
