bbcc755ba1fb1ad69264703dca8ba5caf00fca1c
takurooon <takurokodama0726@gmail.com>
2021-04-20 09:46:09 +0900

    管理画面の公開レポコ数とそのCL数カウントの実装

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,9 +2,12 @@ class Admin::AdminController < Admin::ApplicationController
 
   def home
     @reports = Report.all.includes(:user, :clinic).order("created_at DESC")
+    @released_reports = Report.released.all.includes(:user, :clinic).order("created_at DESC")
     @users = User.all.includes(:reports).order("created_at DESC")
     @clinics = Clinic.all.includes(:reports)
     @clinics_reports = Clinic.joins(:reports).group(:id).order("created_at DESC")
+    @clinics_count = Report.group(:clinic_id).size
+    @clinics_count_released = Report.released.group(:clinic_id).size
   end
 
 end
