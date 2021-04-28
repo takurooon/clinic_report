c744e59b027cd0b2a5af5f751bb15c3c4bbbf16d
takurooon <takurokodama0726@gmail.com>
2021-04-29 00:22:44 +0900

    clinics#clinic_reportに@like_count追加

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -129,6 +129,7 @@ class ClinicsController < ApplicationController
     @reports = Report.where(clinic_id: @clinics.id, status: 0)
     @clinic_reports = Report.where(activated: true).search(params[:search]).order(created_at: :desc)
     @transfer_reports = Report.joins(:itinerary_of_choosing_a_clinics).where(status: 0, itinerary_of_choosing_a_clinics: {clinic_id: @clinics.id, public_status: "show"}).distinct
-    @clinic_reports_count = Report.where(clinic_id: @clinics.id, status: 0).count
+    @clinic_reports_count = Report.where(clinic_id: @clinics.id, status: 0).size
+    @like_count = Like.group(:report_id).size
   end
 end
