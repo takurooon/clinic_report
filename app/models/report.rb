0308e6a93c17e85d485345751855552818e5b89c
takurooon <takurokodama0726@gmail.com>
2021-05-25 23:13:34 +0900

    複合検索の年齢とAMHの選択肢変更(幅), amhのogpの変数修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -162,19 +162,47 @@ class Report < ApplicationRecord
   def self.compound_search(search)
     reports = Report.released.all.includes(:user, :clinic).order("created_at DESC")
     if search[:treatment_end_age].present?
-      reports = reports.where(treatment_end_age: search[:treatment_end_age])
+      age_value = search[:treatment_end_age].delete("^0-9")
+      if age_value.length == 2
+        reports = reports.where(treatment_end_age: age_value)
+      elsif age_value.length == 4
+        i = age_value.scan(/.{2}/)
+        a = i[0]
+        b = i[1]
+        reports = reports.where(treatment_end_age: a..b)
+      elsif age_value.length == 5
+        reports = reports.where(treatment_end_age: 50)
+      else
+        reports = reports.where(treatment_end_age: age_value)
+      end
     end
     if search[:fertility_treatment_number].present?
-    reports = reports.where(fertility_treatment_number: search[:fertility_treatment_number])
+      reports = reports.where(fertility_treatment_number: search[:fertility_treatment_number])
     end
     if search[:amh].present?
-    reports = reports.where(amh: search[:amh])
+      amh_value = search[:amh].delete("^0-9")
+      if amh_value.length <= 3
+        reports = reports.where(amh: amh_value)
+      elsif amh_value.length == 4
+        i = amh_value.scan(/.{2}/)
+        a = i[0]
+        b = i[1]
+        reports = reports.where(amh: a..b)
+      elsif amh_value.length == 5
+        reports = reports.where(amh: [95, 100])
+      elsif amh_value.length == 6
+        reports = reports.where(amh: [6, 7, 8, 9, 10])
+      elsif amh_value.length == 7
+        reports = reports.where(amh: [1, 2, 3, 4, 5])
+      else
+        reports = reports.where(amh: "1000")
+      end
     end
     if search[:type_of_ovarian_stimulation].present?
-    reports = reports.where(type_of_ovarian_stimulation: search[:type_of_ovarian_stimulation])
+      reports = reports.where(type_of_ovarian_stimulation: search[:type_of_ovarian_stimulation])
     end
     if search[:ishoku_type].present?
-    reports = reports.where(ishoku_type: search[:ishoku_type])
+      reports = reports.where(ishoku_type: search[:ishoku_type])
     end
     return reports
   end
